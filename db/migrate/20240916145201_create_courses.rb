d98e6973feaec5d90e3b23e61ac790d8b02ca509
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-17 00:25:23 +0900

    migrated db

--- db/migrate/20240916145201_create_courses.rb
+++ db/migrate/20240916145201_create_courses.rb
@@ -1,7 +1,24 @@
 class CreateCourses < ActiveRecord::Migration[7.2]
   def change
-    create_table :courses do |t|
-      t.string :record_key
+    create_table :courses, id: :uuid do |t|
+      t.string :record_key, null: false
+      t.string :department, null: false
+      t.integer :course_number, null: false
+      t.integer :section, null: false
+      t.integer :crn, null: false
+      t.string :title, null: false
+      t.string :class_type, null: false
+      t.integer :room_number
+      t.boolean :has_secured_room
+      t.jsonb :schedule
+      t.string :semester, null: false
+      t.integer :year, null: false
+      t.integer :credits, null: false
+      t.string :label, null: false
+      t.string :status, default: "new"
+      t.string :instructor_id, null: false
+      t.text :special_info
+      t.text :notes
 
       t.timestamps
     end
