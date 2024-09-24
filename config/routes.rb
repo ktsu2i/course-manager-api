3ec8acf7aed919f3b8535441b10584dd0bd3b5e4
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-24 22:53:26 +0900

    installed devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount_devise_token_auth_for "User", at: "auth"
   namespace :api do
     resources :courses, only: [ :index, :show, :create, :update, :destroy ]
   end
--- db/migrate/20240916145201_create_courses.rb
+++ db/migrate/20240916145201_create_courses.rb
@@ -1,4 +1,4 @@
-class CreateCourses < ActiveRecord::Migration[7.2]
+class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses, id: :uuid do |t|
       t.string :record_key, null: false
