89be08c954850978e15402a57a3d77abfe165755
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-09-27 01:40:11 +0900

    changed User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,7 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :validatable,
-                    :confirmable
-            include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+  include DeviseTokenAuth::Concerns::User
 end
